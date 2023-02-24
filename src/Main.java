078dae397f7d4ca10488e300a65740085daedc5c
mukesh007kls <mallikanti2000@gmail.com>
2023-02-24 09:19:46 +0530

    ADDED:Code for Rolling a Dice

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
         int startingPosition=0;
+        int rollingDice=(int) (Math.random()*10)%6 + 1;
+        System.out.println(rollingDice);
     }
 }
