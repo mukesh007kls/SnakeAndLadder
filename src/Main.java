73306406d8ee9e39761c6200a8a5bf0b5c58410a
mukesh007kls <mallikanti2000@gmail.com>
2023-02-24 09:30:48 +0530

    ADDED:Code so the player reaches Exactly win position

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
-    public static int WIN_POSITION = 100;
+    public static final int WIN_POSITION = 100;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
         int startPosition = 0;
 
-        while (startPosition <= 100) {
+        while (startPosition <= WIN_POSITION) {
             int rollingDice = (int) (Math.random() * 10) % 6 + 1;
             //Snake or ladder option--sol
             int sol = (int) (Math.random() * 10) % 3;
@@ -15,6 +15,9 @@ public class Main {
             switch (sol) {
                 case LADDER:
                     startPosition += rollingDice;
+                    if(startPosition>WIN_POSITION){
+                        startPosition-=rollingDice;
+                    }
                     break;
                 case SNAKE:
                     startPosition -= rollingDice;
@@ -25,7 +28,13 @@ public class Main {
                 startPosition=0;
             }
 
+            if(startPosition==WIN_POSITION){
+                System.out.println("Player Reached Win Position");
+                break;
+            }
+
             System.out.println("Position of player:-"+startPosition);
         }
+
     }
 }
