940de61207bfde6f6f40c428d973eb5820e2ba34
mukesh007kls <mallikanti2000@gmail.com>
2023-02-24 09:36:34 +0530

    ADDED:Code to find Number Of Times Dice is Rolled

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
         int startPosition = 0;
+        //number of time dice rolled -- notdr
+        int notdr = 0;
 
         while (startPosition <= WIN_POSITION) {
             int rollingDice = (int) (Math.random() * 10) % 6 + 1;
+            notdr++;
             //Snake or ladder option--sol
             int sol = (int) (Math.random() * 10) % 3;
 
             switch (sol) {
                 case LADDER:
                     startPosition += rollingDice;
-                    if(startPosition>WIN_POSITION){
-                        startPosition-=rollingDice;
+                    if (startPosition > WIN_POSITION) {
+                        startPosition -= rollingDice;
                     }
                     break;
                 case SNAKE:
                     startPosition -= rollingDice;
-                    if(startPosition<0){
-                        startPosition=0;
+                    if (startPosition < 0) {
+                        startPosition = 0;
                     }
                     break;
             }
 
-            if(startPosition==WIN_POSITION){
-                System.out.println("Player Reached Win Position");
+
+
+            if (startPosition == WIN_POSITION) {
+                System.out.println("Player Reached Win Position:- " + startPosition + " Number of time dice rolled:- " + notdr);
                 break;
             }
 
-            System.out.println("Position of player:-"+startPosition);
+            System.out.println("Position of player:- " + startPosition + " Number of time dice rolled:- " + notdr);
         }
 
     }
