d31d73fe2c513284c767eb4cd53ad63c5f26a401
mukesh007kls <mallikanti2000@gmail.com>
2023-02-24 09:24:41 +0530

    ADDED:Code to Check For Snake And Ladder

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
+    public static final int LADDER=1;
+    public static final int SNAKE=2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
-        int startingPosition=0;
+        int startPosition=0;
         int rollingDice=(int) (Math.random()*10)%6 + 1;
-        System.out.println(rollingDice);
+        //Snake or ladder option--sol
+        int sol=(int) (Math.random()*10)%3;
+        switch (sol){
+            case LADDER:
+                startPosition+=rollingDice;
+                break;
+            case SNAKE:
+                startPosition-=rollingDice;
+                break;
+        }
+        System.out.println(startPosition);
     }
 }
